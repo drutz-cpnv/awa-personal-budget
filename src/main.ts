770c1e5b1941a78410618703075a25a22eca51bb
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-12-02 15:58:51 +0100

    refactor: add the router to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
-import Router from './routes.ts'
 
-createApp(App)
-    .use(Router)
-    .mount('#app')
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
