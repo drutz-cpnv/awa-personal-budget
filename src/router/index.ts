139adcc8e67c8755e4911caa7b75fef1f89436ed
NoahDelgado <noah.delgado1@eduvaud.ch>
2024-12-16 15:19:30 +0100

    feet: add Transactions route to the router

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Home from '../components/Home.vue'
 import Budgets from '../components/Budgets.vue'
 import Income from '../components/Income.vue'
 import Expenses from '../components/Expenses.vue'
+import Transactions from '../components/Transactions.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -30,6 +31,11 @@ const router = createRouter({
             path: '/expenses',
             name: 'expenses',
             component: Expenses
+        },
+        {
+            path: '/Transactions',
+            name: 'transactions',
+            component: Transactions
         }
     ],
 })
